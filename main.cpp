697e8d12b8cfad63aa84d445e9dacf5863602458
misaki-kiko <misaki@futa.in>
2017-08-26 14:15:16 +0800

    null

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "f**k you" << std::endl;
+    std::cout << "thank you sir" << std::endl;
     return 0;
 }
